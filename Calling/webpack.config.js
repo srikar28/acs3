6d0a796c781e046cfc3bfea04176a5a7b92bd135
srikar28 <srikar.38@gmail.com>
2025-03-19 23:17:05 +0400

    commit2

--- Calling/webpack.config.js
+++ Calling/webpack.config.js
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 /* eslint-disable @typescript-eslint/no-var-requires */
+import * as path from 'path';
 
 module.exports = (env) => {
   const babelConfig = require('./.babelrc.js');
